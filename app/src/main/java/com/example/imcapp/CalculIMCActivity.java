5d6a62b33c991cc03f0e466fcfa87eb465f1a8a7
nguetcheu <dnguetcheu@gmail.com>
2022-01-16 14:31:50 +0100

    Application de calcul imc en java mobile

---
+++ app/src/main/java/com/example/imcapp/CalculIMCActivity.java
@@ -0,0 +1,107 @@
+package com.example.imcapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.NumberPicker;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+public class CalculIMCActivity extends AppCompatActivity {
+    private LinearLayout layout;
+    private RelativeLayout relativeLayout;
+
+    private EditText edtNom;
+    private EditText edtPre;
+
+    private NumberPicker numberPickerTaille;
+    private NumberPicker numberPickerPoids;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calcul_imcactivity);
+
+        layout = findViewById(R.id.linearform);
+        relativeLayout = (RelativeLayout) layout.findViewById(R.id.relativeLayout);
+
+
+        edtNom =(EditText) layout.findViewById(R.id.edtnom);
+        edtPre =(EditText) layout.findViewById(R.id.edtPrenom);
+
+        //////////////////////////       GESTION DES NUMBER PICKER ET DE LEUR LOGIQUE //////////////////////////////
+        // Gestion du number picker
+
+        numberPickerTaille = (NumberPicker) relativeLayout.findViewById(R.id.numberPickerTaille);
+        numberPickerPoids = (NumberPicker) relativeLayout.findViewById(R.id.numberPickerPoids);
+
+        // Definition des valeurs minimale des NumberPicker
+        numberPickerTaille.setMinValue(1);
+        numberPickerPoids.setMinValue(1);
+
+            // Definition des valeurs maximale des NumberPicker
+        numberPickerTaille.setMaxValue(203);
+        numberPickerPoids.setMaxValue(190);
+
+
+        // Définition du composant avec les troits points(...)
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+
+    // Remplissage des options du menu trois points(...) et affichage
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add("Calculer");
+        menu.add("Quitter");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+
+    // Redirection vers l'activite d'affichage de l'imc
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+
+        if (item.getTitle().equals("Calculer")){
+            String nom = edtNom.getText().toString();
+            String prenom = edtPre.getText().toString();
+
+            int poids = numberPickerPoids.getValue();
+            int taille = ( numberPickerTaille.getValue()  * numberPickerTaille.getValue() ) / 10000;
+            int imc = (poids / taille) -4;
+
+
+            Intent intent = new Intent(CalculIMCActivity.this, IMCActivity.class);
+
+            // Crer un bundle
+            Bundle bundle = new Bundle();
+
+            // Ajouter le nom du bundle
+            bundle.putString("prenom", prenom);
+            bundle.putString("nom",nom);
+            bundle.putInt("imc", imc);
+
+            // Ajouter le bundle a l'intention
+            intent.putExtras(bundle);
+
+            // Démarrer l'activite accueil
+            startActivity(intent);
+        }
+        else if (item.getTitle().equals("Quitter")){
+            // Fermeture de l'application
+            this.finishAffinity();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/imcapp/IMCActivity.java
@@ -0,0 +1,66 @@
+package com.example.imcapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.MessageFormat;
+
+public class IMCActivity extends AppCompatActivity {
+    private TextView txtNom;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_imcactivity);
+
+        // Recuperer les widgets xml
+        txtNom =(TextView) findViewById(R.id.txtWelcome);
+
+        String Newligne=System.getProperty("line.separator");
+        String etatSante;
+        /*
+        // Recuperer l'intention
+        Intent intent = getIntent();
+         */
+
+        // Recuperer le bundle
+        Bundle bundle = getIntent().getExtras();
+
+        // Recuperer imc
+        String nom = bundle.getString("nom");
+        String prenom = bundle.getString("prenom");
+        int imc = bundle.getInt("imc");
+
+        ///////////////////////////////// etat de sante particulier //////////////////////////////
+        if (imc < 16.5) {
+            etatSante = "denutrition ou famine";
+        }
+        else if (imc>16.5 && imc <18.5) {
+            etatSante = "migreur";
+        }
+        else if (imc>18.5 && imc <25) {
+            etatSante = "corpulence normale";
+        }
+        else if (imc>25 && imc <30) {
+            etatSante = "surpoids";
+        }
+        else if (imc>30 && imc <35) {
+            etatSante = "obesite moderée";
+        }
+        else if (imc>35 && imc <40) {
+            etatSante = "obesite sévère";
+        }
+        else {
+            etatSante = "obesite morbide ou massive";
+        }
+
+        ///////////////////////////////// etat de sante particulier //////////////////////////////
+
+        // Afficher le nom dans la textView
+        txtNom.setText(MessageFormat.format("LE CALCUL DE VOTRE IMC \r \r Monsieur {0} {1} \r Votre imc est egale a : {2}                                   Vous etes en {3} ", nom, prenom, imc, etatSante));
+    }
+
+}
